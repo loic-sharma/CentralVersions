206400365fab70d483ebd560d222f66581f0373d
Loic Sharma <sharma.loic@gmail.com>
2020-03-17 11:53:11 -0700

    Init

---
+++ Inner/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Inner
+{
+    public class Class1
+    {
+        public static void Test()
+        {
+            JsonConvert.DeserializeObject<List<string>>("['test', 'test']");
+        }
+    }
+}
